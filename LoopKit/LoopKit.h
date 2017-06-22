7b27b34693bb1935e5e1ecbf5ee6e749439285aa
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-21 21:28:51 -0400

    Setup general project structure

---
+++ LoopKit/LoopKit.h
@@ -0,0 +1,19 @@
+//
+//  LoopKit.h
+//  LoopKit
+//
+//  Created by Romain Pouclet on 2017-06-21.
+//  Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoopKit.
+FOUNDATION_EXPORT double LoopKitVersionNumber;
+
+//! Project version string for LoopKit.
+FOUNDATION_EXPORT const unsigned char LoopKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoopKit/PublicHeader.h>
+
+
