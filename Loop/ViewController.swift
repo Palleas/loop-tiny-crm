a5ca567ca3d0a60430130693c504d38090aa2c31
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-21 21:28:00 -0400

    Initial Commit

---
+++ Loop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loop
+//
+//  Created by Romain Pouclet on 2017-06-21.
+//  Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
