22e59c2abf4e0a6f4ee54e8d4bd21785012b214f
Romain Pouclet <romain.pouclet@gmail.com>
2017-07-22 11:48:07 -0400

    Setup spec for app coordinator

---
+++ LoopTests/AppCoordinatorSpec.swift
@@ -0,0 +1,16 @@
+//
+//  AppCoordinatorSpec.swift
+//  Loop
+//
+//  Created by Romain Pouclet on 2017-07-22.
+//Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class AppCoordinatorSpec: QuickSpec {
+    override func spec() {
+
+    }
+}
