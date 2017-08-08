d3e35af6877a649d543f896b84ebc99e6badea1c
Romain Pouclet <romain.pouclet@gmail.com>
2017-08-07 22:00:03 -0400

    Add missing title

--- Loop/ViewControllers/SelectActivityViewController.swift
+++ Loop/ViewControllers/SelectActivityViewController.swift
@@ -19,6 +19,8 @@ final class SelectActivityViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        self.title = "Remind Yourself"
+
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(didTapDone))
     }
 
